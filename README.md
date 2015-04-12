f58e95f11f2fc2cf804c42f481748edf8f332881
wzup <weiss10700@gmail.com>
2015-04-12 16:38:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sample LoopBack Application. Scaffolding
+
+Just cline and you're ready to go.
+
+Perfect for quick deploys for playing with AngularJS, for example.
+
+I already has root page client/index.html. Just add some static assets ang go.
---
+++ common/models/coffee-shop.js
@@ -0,0 +1,24 @@
+module.exports = function(CoffeeShop) {
+    // 'use strict';
+    CoffeeShop.status = function(cb) {
+        var currentDate = new Date(),
+            currentHour = currentDate.getHours(),
+            OPEN_HOUR = 6,
+            CLOSE_HOUR = 20,
+            response;
+
+        console.log('currentHour is:', currentHour);
+
+        if(currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+            response = 'We are open for business. Current Hour it is ' + currentHour;
+        } else {
+            response = 'Sorry, we are closed. Open daily from 6am to 8pm. Current Hour it is ' + currentHour;
+        }
+        cb(null, response);
+    };
+
+    CoffeeShop.remoteMethod('status', {
+        http: {path: '/status', verb: 'get'},
+        returns: {arg: 'status', type: 'string'}
+    });
+};
